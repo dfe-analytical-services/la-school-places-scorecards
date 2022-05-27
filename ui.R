4f289ad16481b3e0e9451f4ba62a53834050553e
CARDIN-STEWART <Simone.CARDIN-STEWART@education.gov.uk>
2022-05-27 10:44:52 +0100

    Added label to download button

--- ui.R
+++ ui.R
@@ -40,6 +40,7 @@ function(request) {
           ),
           br(),
           br(),
+          p("Download data for all geographies and phases using the button below."),
           myDownloadButton(
             "download_ud",
             "Download data"
