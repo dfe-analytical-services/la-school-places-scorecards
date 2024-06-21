1997a2421139983529e77a67fff42986d1250539
Rich Bielby <richard.bielby@education.gov.uk>
2024-06-21 17:24:21 +0100

    Updated packages in renv and fixed minor issue in Rmd file

--- server.R
+++ server.R
@@ -10,7 +10,6 @@ function(input, output, session) {
   output$cookie_status <- dfeshiny::cookie_banner_server(
     "cookies",
     input_cookies = reactive(input$cookies),
-    input_clear = reactive(input$cookie_consent_clear),
     parent_session = session,
     google_analytics_key = google_analytics_key
   )
