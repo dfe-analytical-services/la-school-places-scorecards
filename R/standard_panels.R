83fc856c3ca677823fa7f7c6d509631f6693bc60
Rich Bielby <richbielby@e.email>
2025-06-26 20:59:25 +0100

    Air styled scripts

--- R/standard_panels.R
+++ R/standard_panels.R
@@ -12,24 +12,47 @@ panel_accessibility <- function() {
       referrer = "no-referrer"
     ),
     h2("Accessibility statement"),
-    br("This accessibility statement applies to the Local Authority (LA) scorecards application.
+    br(
+      "This accessibility statement applies to the Local Authority (LA) scorecards application.
             This application is run by the Department for Education. We want as many people as possible to be able to use this application,
-            and have actively developed this application with accessibilty in mind."),
+            and have actively developed this application with accessibilty in mind."
+    ),
     h3("WCAG 2.1 compliance"),
-    br("We follow the reccomendations of the ", a(href = "https://www.w3.org/TR/WCAG21/", "WCAG 2.1 requirements. "), "This application has been checked using the ", a(href = "https://github.com/ewenme/shinya11y", "Shinya11y tool "), ", which did not detect accessibility issues.
-             This application also fully passes the accessibility audits checked by the ", a(href = "https://developers.google.com/web/tools/lighthouse", "Google Developer Lighthouse tool"), ". This means that this application:"),
+    br(
+      "We follow the reccomendations of the ",
+      a(href = "https://www.w3.org/TR/WCAG21/", "WCAG 2.1 requirements. "),
+      "This application has been checked using the ",
+      a(href = "https://github.com/ewenme/shinya11y", "Shinya11y tool "),
+      ", which did not detect accessibility issues.
+             This application also fully passes the accessibility audits checked by the ",
+      a(
+        href = "https://developers.google.com/web/tools/lighthouse",
+        "Google Developer Lighthouse tool"
+      ),
+      ". This means that this application:"
+    ),
     tags$div(tags$ul(
       tags$li("uses colours that have sufficient contrast"),
-      tags$li("allows you to zoom in up to 300% without the text spilling off the screen"),
-      tags$li("has its performance regularly monitored, with a team working on any feedback to improve accessibility for all users")
+      tags$li(
+        "allows you to zoom in up to 300% without the text spilling off the screen"
+      ),
+      tags$li(
+        "has its performance regularly monitored, with a team working on any feedback to improve accessibility for all users"
+      )
     )),
     h3("Limitations"),
-    br("We recognise that there are still potential issues with accessibility in this application, but we will continue
+    br(
+      "We recognise that there are still potential issues with accessibility in this application, but we will continue
              to review updates to technology available to us to keep improving accessibility for all of our users. For example, these
-            are known issues that we will continue to monitor and improve:"),
+            are known issues that we will continue to monitor and improve:"
+    ),
     tags$div(tags$ul(
-      tags$li("Keyboard navigation through the interactive charts is currently limited"),
-      tags$li("Alternative text in interactive charts is limited to titles and could be more descriptive (although this data is available in csv format)")
+      tags$li(
+        "Keyboard navigation through the interactive charts is currently limited"
+      ),
+      tags$li(
+        "Alternative text in interactive charts is limited to titles and could be more descriptive (although this data is available in csv format)"
+      )
     )),
     h3("Feedback"),
     br(
